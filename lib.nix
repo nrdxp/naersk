4d8dafe94f45f1d6554a03c3fce00903fef52c2b
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-01-08 22:29:40 +0300

    fix: no rev found

--- lib.nix
+++ lib.nix
@@ -13,33 +13,36 @@ rec
   mkVersions = cargolock:
     if builtins.hasAttr "metadata" cargolock then
 
-      # TODO: this should nub by <pkg-name>-<pkg-version>
+    # TODO: this should nub by <pkg-name>-<pkg-version>
       (
-        lib.concatMap (
-          x:
+        lib.concatMap
+          (
+            x:
             let
               mdk = mkMetadataKey x.name x.version;
             in
-              (
-                lib.optional (builtins.hasAttr mdk cargolock.metadata)
-                  {
-                    inherit (x) version name;
-                    sha256 = cargolock.metadata.${mkMetadataKey x.name x.version};
-                  }
-              ) ++ (lib.concatMap (parseDependency cargolock) (x.dependencies or []))
+            (
+              lib.optional (builtins.hasAttr mdk cargolock.metadata)
+                {
+                  inherit (x) version name;
+                  sha256 = cargolock.metadata.${mkMetadataKey x.name x.version};
+                }
+            ) ++ (lib.concatMap (parseDependency cargolock) (x.dependencies or [ ]))
 
-        )
+          )
           cargolock.package
       )
     else if builtins.hasAttr "package" cargolock then
-      map (
-        p:
+      map
+        (
+          p:
           {
             inherit (p) name version;
             sha256 = p.checksum;
           }
-      ) (builtins.filter (builtins.hasAttr "checksum") cargolock.package)
-    else [];
+        )
+        (builtins.filter (builtins.hasAttr "checksum") cargolock.package)
+    else [ ];
 
   # Turns "lib-name lib-ver (registry+...)" to [ { name = "lib-name", etc } ]
   # iff the package is present in the Cargo.lock (otherwise returns [])
@@ -50,13 +53,13 @@ rec
       version = lib.elemAt components 1;
       mdk = mkMetadataKey name version;
     in
-      lib.optional (builtins.hasAttr mdk cargolock.metadata)
-        (
-          let
-            sha256 = cargolock.metadata.${mkMetadataKey name version};
-          in
-            { inherit name version sha256; }
-        );
+    lib.optional (builtins.hasAttr mdk cargolock.metadata)
+      (
+        let
+          sha256 = cargolock.metadata.${mkMetadataKey name version};
+        in
+        { inherit name version sha256; }
+      );
 
 
   # crafts the key used to look up the sha256 in the cargo lock; no
@@ -78,7 +81,7 @@ rec
     { cargotomls
     , cargolock
     }:
-      let
+    let
       tomlDependencies = cargotoml:
         lib.filter (x: ! isNull x) (
           lib.mapAttrsToList
@@ -116,8 +119,8 @@ rec
                   });
                 }
             ) cargotoml.dependencies or { });
-      in
-        lib.mapAttrs (_: tomlDependencies) cargotomls;
+    in
+    lib.mapAttrs (_: tomlDependencies) cargotomls;
 
   # A very minimal 'src' which makes cargo happy nonetheless
   dummySrc =
@@ -127,69 +130,69 @@ rec
     , copySources # list of paths that should be copied to the output
     , copySourcesFrom # path from which to copy ${copySources}
     }:
-      let
-        config = writeText "config" cargoconfig;
-        cargolock' = builtinz.writeTOML "Cargo.lock" cargolock;
-        fixupCargoToml = cargotoml:
-          let
-            attrs =
-              # Since we pretend everything is a lib, we remove any mentions
-              # of binaries
-              removeAttrs cargotoml [ "bin" "example" "lib" "test" "bench" "default-run" ]
-                // lib.optionalAttrs (builtins.hasAttr "package" cargotoml) ({ package = removeAttrs cargotoml.package [ "default-run" ] ; })
-                ;
-          in
-            attrs // lib.optionalAttrs (lib.hasAttr "package" attrs) {
-              package = removeAttrs attrs.package [ "build" ];
-            };
+    let
+      config = writeText "config" cargoconfig;
+      cargolock' = builtinz.writeTOML "Cargo.lock" cargolock;
+      fixupCargoToml = cargotoml:
+        let
+          attrs =
+            # Since we pretend everything is a lib, we remove any mentions
+            # of binaries
+            removeAttrs cargotoml [ "bin" "example" "lib" "test" "bench" "default-run" ]
+            // lib.optionalAttrs (builtins.hasAttr "package" cargotoml) ({ package = removeAttrs cargotoml.package [ "default-run" ]; })
+          ;
+        in
+        attrs // lib.optionalAttrs (lib.hasAttr "package" attrs) {
+          package = removeAttrs attrs.package [ "build" ];
+        };
 
-        # a list of tuples from member to cargo toml:
-        #   "foo-member:/path/to/toml bar:/path/to/other-toml"
-        cargotomlss = lib.mapAttrsToList
-          (k: v: "${k}:${builtinz.writeTOML "Cargo.toml" (fixupCargoToml v)}")
-          cargotomls;
+      # a list of tuples from member to cargo toml:
+      #   "foo-member:/path/to/toml bar:/path/to/other-toml"
+      cargotomlss = lib.mapAttrsToList
+        (k: v: "${k}:${builtinz.writeTOML "Cargo.toml" (fixupCargoToml v)}")
+        cargotomls;
 
-      in
-        runCommand "dummy-src"
-          { inherit copySources copySourcesFrom cargotomlss; }
-          ''
-            mkdir -p $out/.cargo
-            ${lib.optionalString (! isNull cargoconfig) "cp ${config} $out/.cargo/config"}
-            cp ${cargolock'} $out/Cargo.lock
+    in
+    runCommand "dummy-src"
+      { inherit copySources copySourcesFrom cargotomlss; }
+      ''
+        mkdir -p $out/.cargo
+        ${lib.optionalString (! isNull cargoconfig) "cp ${config} $out/.cargo/config"}
+        cp ${cargolock'} $out/Cargo.lock
 
-            for p in $copySources; do
-              echo "Copying patched source $p to $out..."
-              # Create all the directories but $p itself, so `cp -R` does the
-              # right thing below
-              mkdir -p "$out/$(dirname "$p")"
-              cp --no-preserve=mode -R "$copySourcesFrom/$p" "$out/$p"
-            done
+        for p in $copySources; do
+          echo "Copying patched source $p to $out..."
+          # Create all the directories but $p itself, so `cp -R` does the
+          # right thing below
+          mkdir -p "$out/$(dirname "$p")"
+          cp -R "$copySourcesFrom/$p" "$out/$p"
+        done
 
-            for tuple in $cargotomlss; do
-                member="''${tuple%%:*}"
-                cargotoml="''${tuple##*:}"
+        for tuple in $cargotomlss; do
+            member="''${tuple%%:*}"
+            cargotoml="''${tuple##*:}"
 
-                final_dir="$out/$member"
-                mkdir -p "$final_dir"
-                final_path="$final_dir/Cargo.toml"
-                cp $cargotoml "$final_path"
+            final_dir="$out/$member"
+            mkdir -p "$final_dir"
+            final_path="$final_dir/Cargo.toml"
+            cp $cargotoml "$final_path"
 
-                # make sure cargo is happy
-                pushd $out/$member > /dev/null
-                mkdir -p src
-                touch src/lib.rs
-                # pretend there's a `build.rs`, otherwise cargo doesn't build
-                # the `[build-dependencies]`. Custom locations of build scripts
-                # aren't an issue because we strip the `build` field in
-                # `fixupCargoToml`; so cargo always thinks there's a build
-                # script which is `./build.rs`.
-                echo 'fn main(){}' > build.rs
-                popd > /dev/null
-            done
-          '';
+            # make sure cargo is happy
+            pushd $out/$member > /dev/null
+            mkdir -p src
+            touch src/lib.rs
+            # pretend there's a `build.rs`, otherwise cargo doesn't build
+            # the `[build-dependencies]`. Custom locations of build scripts
+            # aren't an issue because we strip the `build` field in
+            # `fixupCargoToml`; so cargo always thinks there's a build
+            # script which is `./build.rs`.
+            echo 'fn main(){}' > build.rs
+            popd > /dev/null
+        done
+      '';
 
   mkPackages = cargolock:
-    lib.foldl' lib.recursiveUpdate {} (
+    lib.foldl' lib.recursiveUpdate { } (
       map (p: { ${p.name} = { ${p.version} = p; }; })
         cargolock.package
     );
@@ -199,8 +202,8 @@ rec
       packages = mkPackages cargolock;
       package = packages.${name}.${version};
     in
-      lib.optionals (builtins.hasAttr "dependencies" package)
-        (map parseDependency' package.dependencies);
+    lib.optionals (builtins.hasAttr "dependencies" package)
+      (map parseDependency' package.dependencies);
 
   transitiveDeps = cargolock: name: version:
     let
@@ -211,21 +214,21 @@ rec
         };
       packages = mkPackages cargolock;
     in
-      builtins.genericClosure
-        {
-          startSet = [ (wrap packages.${name}.${version}) ];
-          operator = p: map (dep: wrap (packages.${dep.name}.${dep.version})) (
-            (
-              lib.optionals (builtins.hasAttr "dependencies" p.package)
-                (map parseDependency' p.package.dependencies)
-            )
-          );
-        };
+    builtins.genericClosure
+      {
+        startSet = [ (wrap packages.${name}.${version}) ];
+        operator = p: map (dep: wrap (packages.${dep.name}.${dep.version})) (
+          (
+            lib.optionals (builtins.hasAttr "dependencies" p.package)
+              (map parseDependency' p.package.dependencies)
+          )
+        );
+      };
 
   # turns "<package> <version> ..." into { name = <package>, version = <version>; }
   parseDependency' = str:
     let
       components = lib.splitString " " str;
     in
-      { name = lib.elemAt components 0; version = lib.elemAt components 1; };
+    { name = lib.elemAt components 0; version = lib.elemAt components 1; };
 }
