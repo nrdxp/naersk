83a590f3f00eb062136b1e309d07c118b657587b
Yusuf Bera Ertan <y.bera003.06@protonmail.com>
2021-01-08 22:22:42 +0300

    fix: 'no tree object'

--- lib.nix
+++ lib.nix
@@ -115,8 +115,8 @@ rec
                     (throw "No 'rev', 'tag' or 'branch' available to specify key, nor a git revision was found in Cargo.lock");
                   checkout = builtins.fetchGit ({
                     url = val.git;
-                  } // lib.optionalAttrs ((val ? branch) || (val ? tag)) {
-                    allRefs = true;
+                  } // lib.optionalAttrs (k == "specs-idvs") {
+                    ref = "gat-hackery";
                   } // lib.optionalAttrs (val ? rev) {
                     rev = val.rev;
                     ref = val.rev;
